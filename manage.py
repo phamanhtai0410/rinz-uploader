51f9606baafe2880c6bd024958d1450fab213b46
Pham Anh Tai <phamanhtai0410@gmail.com>
2022-11-28 17:14:30 +0700

    erase CORS

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask_cors import CORS
+# from flask_cors import CORS
 
 from gevent import monkey
 monkey.patch_all()
@@ -8,7 +8,7 @@ from src import create_app
 from flask_script import Manager
 
 app = create_app()
-cors = CORS(app, resources={r"/v1/*": {"origins": "*"}})
+# cors = CORS(app, resources={r"/v1/*": {"origins": "*"}})
 manager = Manager(app)
 
 
